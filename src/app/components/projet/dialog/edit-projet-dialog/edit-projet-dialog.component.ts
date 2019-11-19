9051078ae5a93d47846fa49decffcc7f054c8ecd
arthhur <arthur.prodhomme@yahoo.fr>
2019-11-19 14:13:56 +0100

    fix(plan)

--- src/app/components/projet/dialog/edit-projet-dialog/edit-projet-dialog.component.ts
+++ src/app/components/projet/dialog/edit-projet-dialog/edit-projet-dialog.component.ts
@@ -5,12 +5,10 @@ import { Projet } from "src/app/models/projet.model";
 import { ProjetService } from "src/app/services/projet.service";
 import {
   MAT_DATE_LOCALE,
-  DateAdapter,
-  MAT_DATE_FORMATS
+  DateAdapter
 } from "@angular/material/core";
 import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from "@angular/material-moment-adapter";
-import * as moment from "moment";
-import { callApiFree } from 'src/app/core/ApiCall';
+import { callApiFree } from "src/app/core/ApiCall";
 
 @Component({
   selector: "app-edit-projet-dialog",
@@ -27,22 +25,7 @@ export class EditProjetDialogComponent implements OnInit {
   projetForm: FormGroup;
   projet: Projet = new Projet();
 
-<<<<<<< HEAD
   clients = [];
-=======
-  clients = [
-    {
-      id_cli: 1,
-      nom: "Prod",
-      prenom: "Arthur"
-    },
-    {
-      id_cli: 2,
-      nom: "Valjean",
-      prenom: "Jean"
-    }
-  ];
->>>>>>> d1ae2e4a78355bdc9dafd66e7d42c71b88fa6ca3
   // tslint:disable-next-line:max-line-length
   constructor(
     private formBuilder: FormBuilder,
@@ -54,17 +37,10 @@ export class EditProjetDialogComponent implements OnInit {
     this.adapter.setLocale("fr");
   }
 
-<<<<<<< HEAD
   async ngOnInit() {
     this.initForm() ;
     this.projet.id = this.data.id ;
     this.clients = await callApiFree("/client", "GET") ;
-
-=======
-  ngOnInit() {
-    this.initForm();
-    this.projet.id = this.data.id;
->>>>>>> d1ae2e4a78355bdc9dafd66e7d42c71b88fa6ca3
   }
 
   initForm() {
@@ -86,20 +62,4 @@ export class EditProjetDialogComponent implements OnInit {
     this.projet.dateCreation = formValue.dateCreation;
     this.projetService.editProjet(this.projet);
   }
-
-<<<<<<< HEAD
-=======
-  /*formatDate(date: Date) {
-    const monthNames = [
-      "01", "02", "03",
-      "04", "05", "06", "07",
-      "08", "09", "10",
-      "11", "12"
-    ];
-    const day = date.getDate();
-    const monthIndex = date.getMonth();
-    const year = date.getFullYear();
-    return year + "-" + monthNames[monthIndex] + "-" + day;
-  }*/
->>>>>>> d1ae2e4a78355bdc9dafd66e7d42c71b88fa6ca3
 }
